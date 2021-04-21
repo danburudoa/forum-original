1bcbb2c7a3b75f00198ae1f19407e03bb6aa9b21
danburudoa <yumamukai515@gmail.com>
2021-04-21 12:54:42 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "boards#index"
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :boards do
+    resources :comments, only: :create do
+      resources :likes, only: [:create, :destroy]
+    end
+  end
 end
